4f73065a40c36ef06a826f58e9f0adab8cfef631
Mason Bially <mason.bially@gmail.com>
2016-12-05 20:03:04 -0800

    day 2 solution

--- day02/first.jl
+++ day02/first.jl
@@ -2,7 +2,23 @@ include("../util/aoc.jl")
 using AoC;
 
 function aoc_2a(input)
-
+  keypad = [
+  [1 2 3]
+  [4 5 6]
+  [7 8 9]
+  ]
+  code = ""
+  x, y = 2, 2
+  for l in split(input)
+    for c in l
+      if c == 'U' && y != 1; y -= 1 end
+      if c == 'D' && y != 3; y += 1 end
+      if c == 'L' && x != 1; x -= 1 end
+      if c == 'R' && x != 3; x += 1 end
+    end
+    code *= string(keypad[y, x])
+  end
+  return code
 end
 
 print(aoc_2a(@aoc_input))
---
+++ day02/second.jl
@@ -0,0 +1,26 @@
+include("../util/aoc.jl")
+using AoC;
+
+function aoc_2b(input)
+  keypad = [
+  [' ' ' ' '1' ' ' ' ']
+  [' ' '2' '3' '4' ' ']
+  ['5' '6' '7' '8' '9']
+  [' ' 'A' 'B' 'C' ' ']
+  [' ' ' ' 'D' ' ' ' ']
+  ]
+  code = ""
+  x, y = 2, 2
+  for l in split(input)
+    for c in l
+      if c == 'U' && y != 1 && keypad[y-1, x] != ' '; y -= 1 end
+      if c == 'D' && y != 5 && keypad[y+1, x] != ' '; y += 1 end
+      if c == 'L' && x != 1 && keypad[y, x-1] != ' '; x -= 1 end
+      if c == 'R' && x != 5 && keypad[y, x+1] != ' '; x += 1 end
+    end
+    code *= string(keypad[y, x])
+  end
+  return code
+end
+
+print(aoc_2b(@aoc_input))
