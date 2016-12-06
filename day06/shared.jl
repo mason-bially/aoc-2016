c9b7a2ecb0f98a49d1ab5ab0834fdbf0071097a4
Mason Bially <mason.bially@gmail.com>
2016-12-05 23:07:11 -0800

    New char utility library.
    Julia's concept of both arrays and character is annoying in combination. `char2ord` and `ord2char` provide conversions of lowercase alpha values to and from array indicies.

--- day06/shared.jl
+++ day06/shared.jl
@@ -2,16 +2,15 @@ function aoc_6(input, sort_row_func)
   lines = split(input)
   w = length(lines[1])
   a = zeros(UInt32, w, 26)
-  _c_base = Int('a')
   for l in lines
     for (i, x) in enumerate(l)
-      a[i, Int(x) + 1 - _c_base] += 1;
+      a[i, char2ord(x)] += 1;
     end
   end
   s = ""
   for i in 1:w
     top_v = sort_row_func(map(x -> (x[2], x[1]), enumerate(a[i, :])))[2]
-    s *= string(Char(_c_base - 1 + top_v))
+    s *= string(ord2char(top_v))
   end
   return s
 end
--- util/aoc.jl
+++ util/aoc.jl
@@ -3,4 +3,8 @@ module AoC
 include("aoc_input.jl")
 export @aoc_input
 
+include("aoc_char.jl")
+export char2ord
+export ord2char
+
 end
