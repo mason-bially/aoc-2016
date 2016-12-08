6c0c21574471e1c6f6650efb3a7c7793c2fe6d23
Mason Bially <mason.bially@gmail.com>
2016-12-07 20:47:56 -0800

    Refactoring day 4 to use counters.

--- day04/shared.jl
+++ day04/shared.jl
@@ -1,13 +1,14 @@
+using DataStructures
+
 room(line) = match(r"(?P<name>[a-z\-]*)(?P<number>\d*)\[(?P<chksum>\w*)\]", line)
 
 function room_checksum(room)
-  a = zeros(UInt32, 26)
-  foreach(c -> a[alpha2ord(c)] += 1, filter(isalpha, room[:name]))
-  return vec(a) |>
-    enumerate |>
-    _ -> sort(collect(_); by=t->t[2], rev=true) |>
+  return room[:name] |> _ -> filter(isalpha, _) |>
+    collect ∘ counter ∘ collect |>
+    _ -> sort(_; by=t->t[1], rev=false) |>
+    _ -> sort(_; by=t->t[2], rev=true) |>
     _ -> take(_, 5) |>
-    _ -> map(t -> ord2alpha(t[1]), _) |>
+    _ -> map(t -> t[1], _) |>
     join
 end
 
@@ -15,8 +16,14 @@ room_valid(room) = room_checksum(room) == room[:chksum]
 room_value(room) = room_valid(room) ? parse(UInt32, room[:number]) : 0
 room_value(s::AbstractString) = room_value(room(s))
 
+function room_decipher(c::Char, i::Int32)
+  return c != '-' ? ord2alpha((alpha2ord(c; basis=0) + i) % 26; basis=0) : ' '
+end
+
 function room_decipher(room)
   i = parse(Int32, room[:number])
-  return join(map(c -> (c != '-') ? ord2alpha(((alpha2ord(c) - 1 + i) % 26) + 1) : ' ', room[:name]))
+  return room[:name] |>
+    _ -> map(c -> room_decipher(c, i), _) |>
+    join
 end
 room_decipher(s::AbstractString) = room_decipher(room(s))
--- util/aoc_alpha.jl
+++ util/aoc_alpha.jl
@@ -3,8 +3,8 @@
 
 Converts an alpha character to a 1 indexed ordinal.
 """
-function alpha2ord(c::Char) ::Integer
-  return Int(c) + -Int('a') + 1
+function alpha2ord(c::Char; basis::Integer=1) ::Integer
+  return Int(c) + -Int('a') + basis
 end
 
 """
@@ -12,6 +12,6 @@ end
 
 Converts a 1 indexed ordinal to an alpha character.
 """
-function ord2alpha(i::Integer) ::Char
-  return Char(i + Int('a') + -1)
+function ord2alpha(i::Integer; basis::Integer=1) ::Char
+  return Char(i + Int('a') + -basis)
 end
